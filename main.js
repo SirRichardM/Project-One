80d686501c3a5196f66c32f3b822283971f58ee5
SirRichardM <richardmoeb@gmail.com>
2019-11-24 12:18:02 -0500

    Now prints 10 random strains that are searched for

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ window.onload = function () {
   let strainInp = document.querySelector("#strainInp")
   let strain = document.querySelector("#strain")
   let testChild = document.querySelector(".see")
+  let placeHolder = [];
 
   // define variables for input and submit
 
@@ -14,21 +15,25 @@ window.onload = function () {
     console.log(inputVal);
     let nameIn = await axios.get(`https://cors-anywhere.herokuapp.com/strainapi.evanbusse.com/jIF7gVD/strains/search/name/${inputVal}`
       
-      
+    
       
     );
     let strain = nameIn;
     console.log(strain);
-    let strainDes = strain.data[0].desc
-    let strainName = strain.data[0].name
-    let strainRace = strain.data[0].race
-    console.log(strainDes)
-    let tester = document.createElement("h1")
-    tester.className.add = "see"
-    tester.innerHTML = `${strainName} is a ${strainRace}: ${strainDes}`
-    testChild.appendChild(tester)
-
+    let strainS = strain.data
     
+    for (let i = 0; i < 10; i++) {
+      let ss = Math.floor(Math.random()*80)
+      let strainDes = strainS[ss].desc
+      let strainName = strainS[ss].name
+      let strainRace = strainS[ss].race
+      console.log(strainDes)
+      let tester = document.createElement("h1")
+      tester.className.add = "see"
+      tester.innerHTML = `<img src="pebbles.jpg"> <br> ${strainName} is a ${strainRace}: ${strainDes}`
+      testChild.appendChild(tester)
+
+    }
     
 
     
