b26fe4b8c0c41a5143d94cd34b0538570af581cd
SirRichardM <richardmoeb@gmail.com>
2019-11-25 16:44:37 -0500

    Lots of updates

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ window.onload = function () {
   let strain = document.querySelector("#strain")
   let testChild = document.querySelector(".see")
   let placeHolder = [];
+  let flavorInp = document.querySelector("#flavorInp")
+  let flavor = document.querySelector("#flavor")
 
   // define variables for input and submit
 
@@ -30,6 +32,10 @@ window.onload = function () {
     
     for (let i = 0; i < 10; i++) {
       let ss = Math.floor(Math.random() * 10)
+      if (strainS.length < 5) {
+        ss = i;
+        
+      } 
       let strainDes = strainS[ss].desc
       let strainName = strainS[ss].name
       let strainRace = strainS[ss].race
@@ -39,7 +45,7 @@ window.onload = function () {
       console.log(strainDes)
       let tester = document.createElement("h1")
       tester.className.add = "see"
-      tester.innerHTML = `<embed src="${gifs}"> <br> ${strainName} is a ${strainRace}: ${strainDes}`
+      tester.innerHTML = `<embed src="${gifs}"> <br> <span class ="coloo"> ${strainName} </span> is a <span class="coloo2"> ${strainRace} </span>: ${strainDes}`
       testChild.appendChild(tester)
 
     }
@@ -86,6 +92,34 @@ window.onload = function () {
   })
 
   // so this "race" search works but does not log descriptions or effects
-  
+  flavor.addEventListener("click", async function (evt) {
+    evt.preventDefault();
+    let flavorVal = flavorInp.value;
+    console.log(flavorVal)
+    let flava = await axios.get(`https://cors-anywhere.herokuapp.com/strainapi.evanbusse.com/jIF7gVD/strains/search/flavor/${flavorVal}`)
+    console.log(flava)
+    let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${flavorVal}&api_key=VC8tFxRJWVzQj5LrvDTYc0YsEgUm4EhH&limit=10`);
+    console.log(gif)
+    let flavors = flava.data
+    let intro = document.createElement("h3");
+    intro.innerHTML = `Yummmmmmm! Here are some strains that contain an essense of ${flavorVal} . . .`
+    testChild.appendChild(intro);
+    for (let i = 0; i < 10; i++) {
+      let flavName = flavors[i].name
+      let flavRace = flavors[i].race
+      //let ran = Math.floor(Math.random()*10)
+      let giffy = gif.data.data
+      let gifs = giffy[i].embed_url
+      let flavorApp = document.createElement("h3")
+      flavorApp.className.add = "food"
+      flavorApp.innerHTML = `<embed src="${gifs}"> <br> <span class="coloo"> ${flavName} </span> which is a <span class="coloo2"> ${flavRace} </span> strain`
+      testChild.appendChild(flavorApp)
+
+
+    }
+    
+    
+  })
+
 
 }
